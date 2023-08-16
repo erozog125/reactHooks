39dcc03bbfeb95346722ef4041eb3c573996ff67
Mateo Bernal <bernalmateoa@gmail.com>
2023-08-16 10:26:19 -0500

    Add changes in Counter and InputText

--- react-hooks/src/components/01-Counter/Counter.jsx
+++ react-hooks/src/components/01-Counter/Counter.jsx
@@ -4,10 +4,18 @@ export const Counter = () => {
 
   const [count, setCount] = useState(0);
 
+  const handleAdd = () => setCount(count+1)
+  const decrement = () => setCount(count-1)
+  const reset = () => setCount(0)
+
   return (
     <>
-      <p className='text-4xl text-sky-400'>Contador: {count}</p>
-      <button className='w-40 text-center text-sky-700 bg-gray-400 rounded-2xl m-4' onClick={() => setCount(count + 1)}>Incrementar</button>    
+      <p className='text-4xl py-3.5 text-sky-400'>Contador: {count}</p>
+        <div className='flex' id='btns'>
+          <button className='w-40 py-2.5 text-center text-sky-700 bg-gray-400 text-xl rounded-2xl m-6' onClick={handleAdd}>Incrementar</button>
+          <button className='w-40 py-2.5 text-center text-sky-700 bg-gray-400 text-xl rounded-2xl m-6' onClick={decrement}>Decrementar</button>
+          <button className='w-40 py-2.5 text-center text-sky-700 bg-gray-400 text-xl rounded-2xl m-6' onClick={reset}>Resetear</button>
+        </div>
     </>
     
   )
--- react-hooks/src/components/02-InputText/InputText.jsx
+++ react-hooks/src/components/02-InputText/InputText.jsx
@@ -2,16 +2,30 @@ import React, { useState } from 'react';
 
 export const InputText = () => {
   const [text, setText] = useState('');
+  const [color, setColor] = useState('black');
+
+const txt = (valor) => setText(valor.target.value)
+
+const handleInputKeyPress = (event) => {
+  // if (event.key === 'Enter') {
+  //   setColor('red')
+  // }else {
+  //   setColor('black')
+  // }
+  event.code === 'Enter' ? setColor('red') : setColor('black')
+}
+
 
   return (
     <>
-      <p>Texto ingresado: {text}</p>
+      <p className='m-4 text-2xl font-semibold text-black' style={{color}}>Texto ingresado: {text}</p>
       <input
         placeholder='Ingresa el texto'
-        className='w-96 bg-sky-300 p-2'
+        className='w-96 bg-blue-300 p-2  placeholder:text-gray-600 font-semibold'
         type="text"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={txt}
+        onKeyUp={handleInputKeyPress}
       />    
     </>
     
