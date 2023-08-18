912843f80b099bb1f941b0afee73058683b1bd24
mateob24 <bernalmateoa@gmail.com>
2023-08-17 23:16:23 -0500

    branch mateob24

--- react-hooks/src/components/02-InputText/InputText.jsx
+++ react-hooks/src/components/02-InputText/InputText.jsx
@@ -7,26 +7,40 @@ export const InputText = () => {
 const txt = (valor) => setText(valor.target.value)
 
 const handleInputKeyPress = (event) => {
-  // if (event.key === 'Enter') {
-  //   setColor('red')
-  // }else {
-  //   setColor('black')
-  // }
   event.code === 'Enter' ? setColor('red') : setColor('black')
 }
 
+const [newText, newSetText] = useState('')
+// const [btn, ]
+
+const newTxt = (e) => newSetText(e.target.value)
 
   return (
     <>
-      <p className='m-4 text-2xl font-semibold text-black' style={{color}}>Texto ingresado: {text}</p>
-      <input
-        placeholder='Ingresa el texto'
-        className='w-96 bg-blue-300 p-2  placeholder:text-gray-600 font-semibold'
-        type="text"
-        value={text}
-        onChange={txt}
-        onKeyUp={handleInputKeyPress}
-      />    
+      <div>
+        <p className='m-4 text-2xl font-semibold text-black' style={{color}}>Texto ingresado: {text}</p>
+        <input
+          placeholder='Ingresa el texto'
+          className='w-96 bg-blue-300 p-2  placeholder:text-gray-600 font-semibold'
+          type="text"
+          value={text}
+          onChange={txt}
+          onKeyUp={handleInputKeyPress}
+        />
+      </div>
+      <div>
+        <p className='m-4 text-2xl font-semibold text-black' style={{color}}>Texto a cambiar: {newText}</p>
+        <input
+          placeholder='Ingresa el texto'
+          className='w-96 bg-blue-300 p-2  placeholder:text-gray-600 font-semibold'
+          type="text"
+          value={newText}
+          onClick={newTxt}
+          onKeyUp={handleInputKeyPress}
+          ont
+        />
+        {/* <button value={}>Enviar texto</button> */}
+      </div>
     </>
     
   );
