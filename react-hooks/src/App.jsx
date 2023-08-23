17622ceb22f2477966a65310cc5b208bfb893f39
svargasc <svc.vargas03@gmail.com>
2023-08-23 10:21:40 -0500

    add

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,13 +1,11 @@
-import { Counter } from "./components/01-Counter/Counter"
-import { InputText } from "./components/02-InputText/InputText"
-import { ToogleState } from './components/03-ToogleState/ToogleState'
-import { Task } from "./components/Tasks/Tasks"
+
+import { Counter } from "./components/Counter/Counter"
 
 function App() {
   
   return (
     <div className='w-full h-auto bg-zinc-700 flex flex-col justify-center items-center'>
-      <Task/>
+      <Counter></Counter>
     </div>
   )
 }
--- react-hooks/src/components/Counter/Counter.jsx
+++ react-hooks/src/components/Counter/Counter.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect ,useState } from 'react'
 
 export const Counter = () => {
 
+  const ClasA = 'text-blue-600'
+  const ClasB = 'text-purple-700'
+
   const [count, setCount] = useState(0);
+  const [styleColor, setStyleColor] = useState(ClasA)
+
+  useEffect(() => {
+    if(count >= 10) {
+      setStyleColor(ClasB);
+    }else{
+      setStyleColor(ClasA)
+    }
+  }),[count]
+  
+
 
   return (
     <>
-      <p className='text-4xl text-sky-400'>Contador: {count}</p>
+      <p className={styleColor}>Contador: {count}</p>
       <button className='w-40 text-center text-sky-700 bg-gray-400 rounded-2xl m-4' onClick={() => setCount(count + 1)}>Incrementar</button>    
       <button className='w-40 text-center text-sky-700 bg-gray-400 rounded-2xl m-4' onClick={() => setCount(count - 1)}>Decrementar</button>    
       <button className='w-40 text-center text-sky-700 bg-gray-400 rounded-2xl m-4' onClick={() => setCount(0)}>Setear</button>    
