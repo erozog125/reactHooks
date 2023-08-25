179d41a095979182b66eacd3bcc1113245d663d5
mauro <andrekxus299@gmail.com>
2023-08-25 11:29:58 -0500

    jejej

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -2,15 +2,17 @@ import { Counter } from "./components/01-Counter/Counter"
 import { InputText } from "./components/02-InputText/InputText"
 import { ToogleState } from './components/03-ToogleState/ToogleState'
 import { Todo } from './components/04-Todo/Todo'
+import { RockPaper } from './components/05-RockPaper/RockPaper'
 
 function App() {
 
   return (
     <div className='w-full bg-gray-600 flex flex-col justify-center items-center h-screen'>
-      <Counter />
+      {/* <Counter /> */}
       {/* <InputText /> */}
       {/* <ToogleState /> */}
       {/* <Todo /> */}
+      <RockPaper />
     </div>
   )
 }
--- react-hooks/src/components/05-RockPaper/RockPaper.jsx
+++ react-hooks/src/components/05-RockPaper/RockPaper.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const RockPaper = () => {
+    const classA = 'text-red-600 text-4x1'
+    const classB = 'text-green-600 text-4x1'
+    const classC = 'text-blue-600 text-4x1'
+
+    const [styleColor, setStyleColor] = useState('black');
+
+    const [userChoice, setUserChoice] = useState('');
+    const [cpuChoice, setCpuChoice] = useState('');
+    const [result, setResult] = useState('');
+
+    const choices = ['Piedra', 'Papel', 'Tijera'];
+
+    const handleChoiceClick = (choice) => {
+        setUserChoice(choice);
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        const randomCpuChoice = choices[randomIndex];
+        setCpuChoice(randomCpuChoice);
+
+        if (choice === randomCpuChoice) {
+            setResult('Empató');
+            setStyleColor(classC);
+        } else if (
+            (choice === 'Piedra' && randomCpuChoice === 'Tijera') ||
+            (choice === 'Papel' && randomCpuChoice === 'Piedra') ||
+            (choice === 'Tijera' && randomCpuChoice === 'Papel')
+        ) {
+            setResult('Ganó');
+            setStyleColor(classB);
+        } else {
+            setResult('Perdió');
+            setStyleColor(classA);
+        }
+    };
     return (
-        <div>RockPaper</div>
+        <div>
+            <h1 className='text-3xl text-sky-600'>Piedra, papel o tijera</h1>
+
+            <h2 className='text-2xl text-sky-600'>tu eleccion es: {userChoice}</h2>
+
+            <h2 className='text-2xl text-sky-600'>cpu: {cpuChoice}</h2>
+
+            <h2 className={`text-xl ${styleColor}`}>Usted: {result}</h2>
+
+            <hr />
+            <article className='flex gap-20 justify-center mt-4'>
+                <button onClick={() => handleChoiceClick('Piedra')}>Piedra</button>
+                <button onClick={() => handleChoiceClick('Papel')}>Papel</button>
+                <button onClick={() => handleChoiceClick('Tijera')}>Tijera</button>
+            </article>
+        </div>
     )
 }
